74b3579f631293e2f42c65397cbabea1ddf2bf41
zhaos <zhaoruiyuan581@gmail.com>
2018-02-05 22:14:33 +0800

    Update my_curl.h

--- my_curl.h
+++ my_curl.h
@@ -1,3 +1,45 @@
+#ifndef SQM_CURL_H_INCLUDED
+#define SQM_CURL_H_INCLUDED
+
+#include "_LOG.h"
+#include "curl/curl.h"
+
+#define TIMEOUT 5
+#define MINIMAL_PROGRESS_FUNCTIONALITY_INTERVAL 0.1
+
+/* this is how the CURLOPT_XFERINFOFUNCTION callback works */
+typedef int (*pfunc_xferinfo)(void *p,
+                    curl_off_t dltotal, curl_off_t dlnow,
+                    curl_off_t ultotal, curl_off_t ulnow);
+
+struct progressinfo {
+  CURL *curl;
+  double lasttime;
+//  pfunc_xferinfo xferinfo_callback; //not used
+};
+
+
+/** \brief 全局初始化 参数CURL_GLOBAL_ALL
+ *
+ * \param void
+ * \return int
+ * 成功CURL_OK
+ */
+int sqm_curl_init(void);
+
+/** \brief 全局清除
+ *
+ * \param void
+ * \return void
+ *
+ */
+void sqm_curl_clean(void);
+int sqm_filedownload(const char *requestURL, const char *saveto, \
+                     const char *user, const char *password, \
+                     pfunc_xferinfo xferinfo_callback, void* xinfer_data);
+
+#endif // SQM_CURL_H_INCLUDED
+
 #include "sqm_curl.h"
 #include <string.h>
 #include <unistd.h>
